825626bc3f1ccfa95260ae6d890c5573dd60d728
Javier Caro <jcaro1993@gmail.com>
2020-11-06 09:18:21 -0800

    adding time

--- s.js
+++ s.js
@@ -1,3 +1,34 @@
+var timeEl = document.querySelector("text-light");
+var mainEl = document.getElementById("time-display");
+
+var secondsLeft = 45;
+
+function setTime() {
+    var timerInterval = setInterval(function() {
+        secondsLeft--;
+        timeEl.textContent = secondsLeft + " seconds left till end of quiz.";
+
+        if (secondsLeft === 0) {
+            clearInterval(timerInterval);
+        }
+
+    }, 1000);
+};
+
+function myTimer() {
+    var interval = setInterval(function() {
+        timer.innerHTML = sec + " seconds left";
+        sec--;
+        if (sec == -1) {
+            clearInterval(interval);
+            alert("Time out!!");
+            tryagain();
+        } else if (yourscore.style.display === "block") {
+            clearInterval(interval);
+        }
+    }, 1000);
+};
+
 document.getElementById("form1").onsubmit = function() {
     Hammer = parseInt(document.querySelector('input[name = "Hammer"]:checked').value);
     shield = parseInt(document.querySelector('input[name = "shield"]:checked').value);
